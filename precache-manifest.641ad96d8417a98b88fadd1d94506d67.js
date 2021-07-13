e081a3b7cd934b544941bc4c5bf3f314ebecce3c
ShoaibDev97 <shoaibmuhammadarif@gmail.com>
2021-07-13 12:47:29 +0500

    Updates

---
+++ precache-manifest.641ad96d8417a98b88fadd1d94506d67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78db8b44e9ab4be7810c56f535af7b6b",
+    "url": "/recipe-react/index.html"
+  },
+  {
+    "revision": "998f54d327b14ba99c48",
+    "url": "/recipe-react/static/css/2.60ff825d.chunk.css"
+  },
+  {
+    "revision": "1b81fc2b7642f66aa3b8",
+    "url": "/recipe-react/static/css/main.7552d8e1.chunk.css"
+  },
+  {
+    "revision": "998f54d327b14ba99c48",
+    "url": "/recipe-react/static/js/2.95372ed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-react/static/js/2.95372ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b81fc2b7642f66aa3b8",
+    "url": "/recipe-react/static/js/main.5fb038cf.chunk.js"
+  },
+  {
+    "revision": "4abaef8dffa2035733d0",
+    "url": "/recipe-react/static/js/runtime-main.c4677a56.js"
+  },
+  {
+    "revision": "9615e2362da2c4c37ce56d47cabb9b1d",
+    "url": "/recipe-react/static/media/bg1.9615e236.png"
+  }
+]);
